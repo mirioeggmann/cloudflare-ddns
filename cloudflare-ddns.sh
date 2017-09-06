8e0d6095ff410fee37ce79c4b4d8f26454499bbb
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2017-09-06 23:31:36 +0200

    Update cloudflare-ddns.sh

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -5,7 +5,7 @@
 #RECORD_ID                    = 372e67954025e0ba6aaa6d586b9e0b59
 #EMAIL                        = user@example.com
 #CLOUDFLARE_GLOBAL_API_KEY    = c2547eb745049flc9320b638f5e225cf483cc5cfdda41
-#HOSTNAME                      = example.com
+#HOSTNAME                     = example.com
 
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/ZONE_ID/dns_records/RECORD_ID" \
      -H "X-Auth-Email: EMAIL" \
