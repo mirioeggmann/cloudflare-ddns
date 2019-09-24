b0d595f9c91837f6954b39863511c58519b4eec3
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2019-09-24 21:40:45 +0200

    changed global api key to bearer key for dns edit

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "X-Auth-Email: $AUTH_EMAIL" \
-     -H "X-Auth-Key: $AUTH_KEY" \
+     -H "Authorization: Bearer $AUTH_KEY" \
      -H "Content-Type: application/json" \
      --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\"}"
