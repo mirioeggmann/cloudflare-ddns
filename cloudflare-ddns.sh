010e91b59bc2ed37c9250ad0a8580b36036e5f02
David Liaw <david@liaw.me>
2022-05-18 23:41:20 +1000

    Use x-auth-key and x-auth-email instead of bearer token

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "Authorization: Bearer $AUTH_KEY" \
+     -H "X-Auth-Email: $AUTH_EMAIL" \
+     -H "X-Auth-Key: $AUTH_KEY" \
      -H "Content-Type: application/json" \
      --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\"}"
