f215100461f177b382d1bc8ad01d355741db6369
Elio Lopez <35702545+ElioLopez@users.noreply.github.com>
2022-09-05 12:40:22 +0200

    Initial commit

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [[ -z "${AUTH_EMAIL+x}" ]]; then
+echo "AUTH_EMAIL are not defined, will use API Bearer Token method"
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+     -H "Authorization: Bearer $API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\"}"
+
+else
+echo "AUTH_EMAIL is defined, will use AUTH_KEY Method"
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
      -H "X-Auth-Email: $AUTH_EMAIL" \
      -H "X-Auth-Key: $AUTH_KEY" \
      -H "Content-Type: application/json" \
      --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\"}"
+fi
