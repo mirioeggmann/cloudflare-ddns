04588b716b5692f77e60c4d5b86a4d6bf40c8163
Alexander Bauer <sasha@linux.com>
2023-06-19 22:51:52 +0000

    Reorganize script logic for easier debugging

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-if [[ -z "${AUTH_EMAIL+x}" ]]; then
-echo "AUTH_EMAIL are not defined, will use API Bearer Token method"
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "Authorization: Bearer $API_TOKEN" \
-     -H "Content-Type: application/json" \
-     --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\",\"proxied\":$PROXIED,\"ttl\":1}"
+set -eu
 
-else
-echo "AUTH_EMAIL is defined, will use AUTH_KEY Method"
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "X-Auth-Email: $AUTH_EMAIL" \
-     -H "X-Auth-Key: $AUTH_KEY" \
-     -H "Content-Type: application/json" \
-     --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\",\"proxied\":$PROXIED,\"ttl\":1}"
+if [[ "${DEBUG:-}" == "true" ]]; then # permit debugging
+  echo "Debug mode is on, this will expose secrets on stderr" 1>&2
+  set -x
 fi
+
+# The -v test determines if a variable is set, even if empty. (Bash 4.3+)
+if [[ ! -v AUTH_HEADERS ]]; then
+  if [[ -v API_TOKEN ]]; then # API_TOKEN is set, prefer to use it
+    echo "Using API_TOKEN for authentication" 1>&2
+    AUTH_HEADERS=(-H "Authorization: Bearer $API_TOKEN")
+  elif [[ -v AUTH_EMAIL && -v AUTH_KEY ]]; then
+    echo "Using AUTH_EMAIL/AUTH_KEY for authentication" 1>&2
+    AUTH_HEADERS=(-H "X-Auth-Email: $AUTH_EMAIL" -H "X-Auth-Key: $AUTH_KEY")
+  else
+    echo "Neither API_TOKEN nor AUTH_EMAIL and AUTH_KEY were provided, failing" 1>&2
+    exit 1
+  fi
+fi
+
+: "${IP:=$(curl ifconfig.co)}"
+: "${PROXIED:=false}"
+echo "Using IP: $IP" 1>&2
+
+PAYLOAD="{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"$IP\",\"proxied\":$PROXIED,\"ttl\":1}"
+echo "Sending payload: $PAYLOAD" 1>&2
+
+# Assemble the whole command into an array, then execute.
+COMMAND=(
+  curl
+  -X PUT
+  "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID"
+  "${AUTH_HEADERS[@]}"
+  -H "Content-Type: application/json"
+  --data "$PAYLOAD"
+)
+
+"${COMMAND[@]}"
+echo # end with line break
